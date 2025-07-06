7b479d44b1aa09e25614a61473a686fee66c72be
Devang <deekayedm@gmail.com>
2025-07-06 14:15:35 +0530

    chat page added

---
+++ packages/ui/src/text-input.tsx
@@ -0,0 +1,22 @@
+interface PropType {
+    placeholder: string
+    size: "small" | "big"
+    onChange: any
+}
+
+
+export function TextInput({size, placeholder, onChange }: PropType) {
+    return (
+        <input
+            onChange={onChange}
+            placeholder={placeholder}
+            style={{
+                padding: size === "big" ? 15: 10,
+                margin: size === "big" ? 15: 10,
+                borderColor: "black",
+
+            }}
+        >
+        </input>
+    )
+}
