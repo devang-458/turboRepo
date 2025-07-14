11833b47ef381c389da96c67eef6c0244501cc0a
Devang <deekayedm@gmail.com>
2025-07-14 14:53:22 +0530

    http server added

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { prisma } from "@repo/db/client";
 
 const app = express();
 
@@ -8,6 +9,18 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello sir");
+});
+
+app.post("/signup", async (req, res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+
+  const response = await prisma.user.create({
+    data: {
+      name: name,
+      password: password,
+    },
+  });
 });
 
 app.listen(3002, () => {
